2925a3b5edba518a7e5cfcb45c70a2276ee63acf
RobbeKempsUCLL <113983954+RobbeKempsUCLL@users.noreply.github.com>
2025-12-07 17:03:01 +0100

    added RoomTesting and RoomCommandHandlerTesting

--- src/test/java/be/ucll/craftsmanship/hotel/rooms/application/RoomCommandHandlerTest.java
+++ src/test/java/be/ucll/craftsmanship/hotel/rooms/application/RoomCommandHandlerTest.java
@@ -1,12 +1,66 @@
 package be.ucll.craftsmanship.hotel.rooms.application;
 
+import be.ucll.craftsmanship.hotel.rooms.commands.CreateRoomCommand;
+import be.ucll.craftsmanship.hotel.rooms.director.RoomDirector;
+import be.ucll.craftsmanship.hotel.rooms.domain.Room;
+import be.ucll.craftsmanship.hotel.rooms.domain.RoomType;
+import be.ucll.craftsmanship.hotel.rooms.infrastructure.RoomRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class RoomCommandHandlerTest {
 
+    private RoomRepository roomRepository;
+    private RoomDirector roomDirector;
+    private RoomCommandHandler roomCommandHandler;
+
+    @BeforeEach
+    void setUp() {
+        roomRepository = mock(RoomRepository.class);
+        roomDirector = new RoomDirector();
+        roomCommandHandler = new RoomCommandHandler(roomRepository, roomDirector);
+    }
+
     @Test
-    void createRoom() {
+    void createNormalRoom() {
+        // Given
+        CreateRoomCommand command = new CreateRoomCommand(102, RoomType.NORMAL);
+
+        when(roomRepository.save(any(Room.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // When
+        Room room = roomCommandHandler.createRoom(command);
+
+        // Then
+        assertNotNull(room);
+        assertEquals(102, room.getRoomNumber());
+        assertEquals(RoomType.NORMAL, room.getType());
+        assertNull(room.getAirConditioning());
+        assertNull(room.getWifi());
+
+        verify(roomRepository).save(any(Room.class));
+    }
+
+    @Test
+    void createLuxuryRoom() {
+        // Given
+        CreateRoomCommand command = new CreateRoomCommand(101, RoomType.LUXURY);
+
+        when(roomRepository.save(any(Room.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // When
+        Room room = roomCommandHandler.createRoom(command);
+
+        // Then
+        assertNotNull(room);
+        assertEquals(101, room.getRoomNumber());
+        assertEquals(RoomType.LUXURY, room.getType());
+        assertNotNull(room.getAirConditioning());
+        assertNotNull(room.getWifi());
+
+        verify(roomRepository).save(any(Room.class));
     }
 }
--- src/test/java/be/ucll/craftsmanship/hotel/rooms/domain/RoomTest.java
+++ src/test/java/be/ucll/craftsmanship/hotel/rooms/domain/RoomTest.java
@@ -1,6 +1,10 @@
 package be.ucll.craftsmanship.hotel.rooms.domain;
 
+import be.ucll.craftsmanship.hotel.rooms.components.AirConditioning;
+import be.ucll.craftsmanship.hotel.rooms.components.Wifi;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,25 +12,42 @@ class RoomTest {
 
     @Test
     void getRoomNumber() {
+        Room room = new Room(101, RoomType.NORMAL, null, null, null);
+        assertEquals(101, room.getRoomNumber());
     }
 
     @Test
     void getReservations() {
+        List<Long> reservations = List.of(1L, 2L, 3L);
+        Room room = new Room(101, RoomType.NORMAL, reservations, null, null);
+        assertEquals(reservations, room.getReservations());
     }
 
     @Test
     void getType() {
+        Room room = new Room(101, RoomType.NORMAL, null, null, null);
+        assertEquals(RoomType.NORMAL, room.getType());
     }
 
     @Test
     void getAirConditioning() {
+        AirConditioning airConditioning = new AirConditioning(21);
+        Wifi wifi = new Wifi("HotelWiFi", 100);
+        Room room = new Room(101, RoomType.LUXURY, null, airConditioning, wifi);
+        assertEquals(airConditioning, room.getAirConditioning());
     }
 
     @Test
     void getWifi() {
+        AirConditioning airConditioning = new AirConditioning(21);
+        Wifi wifi = new Wifi("HotelWiFi", 100);
+        Room room = new Room(101, RoomType.LUXURY, null, airConditioning, wifi);
+        assertEquals(wifi, room.getWifi());
     }
 
     @Test
     void getId() {
+        Room room = new Room(101, RoomType.NORMAL, null, null, null);
+        assertNull(room.getId());
     }
 }
